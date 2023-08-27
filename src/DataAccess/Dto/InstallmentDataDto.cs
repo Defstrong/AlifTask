63fc3a7fc895b2df90a449e24cdfc5cf9f07b573
Defstrong <sfayzulloev1@gmail.com>
2023-08-27 16:47:07 +0500

    add enum of product and dto of installment data

---
+++ src/DataAccess/Dto/InstallmentDataDto.cs
@@ -0,0 +1,12 @@
+namespace AlifTask.DataAccess;
+
+public sealed record InstallmentDataDto
+{
+    public decimal Price { get; set; }
+
+    public string PhoneNumberClient { get; set; }
+
+    public int ClientInstallmentRange { get; set; }
+
+    public ProductType ProductType { get; init; }
+}
---
+++ src/DataAccess/Enum/ProductType.cs
@@ -0,0 +1,8 @@
+namespace AlifTask.DataAccess;
+
+public enum ProductType : int
+{
+    Phone = 0,
+    Desktop = 1,
+    TV = 2
+}
